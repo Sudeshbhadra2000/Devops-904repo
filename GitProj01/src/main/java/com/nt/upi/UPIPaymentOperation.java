792b27e5cfdf871003ecef41c18fec21cc3c6eaf
admin <admin@192.168.1.47>
2025-01-17 19:17:50 +0530

    Adding new point on branch1

--- GitProj01/src/main/java/com/nt/upi/UPIPaymentOperation.java
+++ GitProj01/src/main/java/com/nt/upi/UPIPaymentOperation.java
@@ -1,8 +1,13 @@
 package com.nt.upi;
 
+import java.util.Random;
+
 public class UPIPaymentOperation {
 
 	public String doPayment(String upiId, double amount) {
 		return amount+" is transfered to "+upiId;
+	}
+	public String addrewards(String txId) {
+		return txId+" has got "+new Random().nextInt(3000)+" Reward point have come";
 	}
 }
