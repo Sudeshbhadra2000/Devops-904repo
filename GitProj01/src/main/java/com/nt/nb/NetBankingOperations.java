a9369b5d1159341df321d32f2afe8ce66b4e999e
admin <admin@192.168.1.47>
2025-01-21 19:04:30 +0530

    developer added cashback offer

--- GitProj01/src/main/java/com/nt/nb/NetBankingOperations.java
+++ GitProj01/src/main/java/com/nt/nb/NetBankingOperations.java
@@ -5,4 +5,7 @@ public class NetBankingOperations {
 	public String doPayment(long cardNo,double amt) {
 		return amt + " Payment is done with the card :: "+cardNo;
 	}
+	public String cashbackOffer(String txId,double amt) {
+		return amt*0.5+" amount goes as cashback amount";
+	}
 }
