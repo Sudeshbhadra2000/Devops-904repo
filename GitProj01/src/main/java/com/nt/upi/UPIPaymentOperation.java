1c6d9fb7f3b4d0be5539963889f3e96df44df9c1
admin <admin@192.168.1.47>
2025-01-21 18:51:30 +0530

    just correcting this

--- GitProj01/src/main/java/com/nt/upi/UPIPaymentOperation.java
+++ GitProj01/src/main/java/com/nt/upi/UPIPaymentOperation.java
@@ -6,7 +6,7 @@ public class UPIPaymentOperation {
 
 	public String doPayment(String upiId, double amount) {
 		return amount+" is transfered to "+upiId;
-	}
+	}  
 	public String addrewards(String txId) {
 		return txId+" has got "+new Random().nextInt(3000)+" Reward point have come";
 	}
